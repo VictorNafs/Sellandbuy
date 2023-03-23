9785125d583c39b4ec22f5b321adf0373be59ffb
Akvayli06 <benidirifaouzi@gmail.com>
2023-03-23 11:05:43 +0100

    add home

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_164423) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_094329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
