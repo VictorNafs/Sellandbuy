07dfaeda25c8aeb9fc3fe54b13bbd0fe5ecc7459
victor <duprezvictor4@gmail.com>
2023-03-24 17:04:27 +0100

    victor commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :home, only: [:index]
   resources :payments, only: [:new, :create]
 
- 
   resources :items do
     get 'checkout', on: :member
     post 'charge', on: :member
-
     resources :transactions, only: [:create]
   end
+
+  resources :users, only: [:destroy]
 
   root "home#index"
 end
