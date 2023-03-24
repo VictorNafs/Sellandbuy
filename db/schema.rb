43fcbb90a24ad5647503546f8e24c6a553eea322
Akvayli06 <benidirifaouzi@gmail.com>
2023-03-24 17:20:23 +0100

    environement mailer

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_121828) do
     t.string "street"
     t.string "zipcode"
     t.string "city"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
