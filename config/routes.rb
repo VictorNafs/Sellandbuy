f7baf1a7eb2fc08fbe8643086e4be0106b269a2b
victor <duprezvictor4@gmail.com>
2023-03-24 15:00:13 +0100

    victor commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  resources :items
   devise_for :users
   resources :home, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :payments, only: [:new, :create]
-  # Defines the root path route ("/")
-  root "home#index"
 
   resources :items do
     get 'checkout', on: :member
     post 'charge', on: :member
-  end
-  
-  
 
+    resources :transactions, only: [:create]
+  end
+
+  root "home#index"
 end
